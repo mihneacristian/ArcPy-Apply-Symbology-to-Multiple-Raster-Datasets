88f18dc52a984580b645a029308a170bc9ad180e
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.com>
2020-05-28 10:25:33 +0300

    First commit

---
+++ script.py
@@ -0,0 +1,9 @@
+import arcpy
+mxd = arcpy.mapping.MapDocument("CURRENT")
+df = mxd.activeDataFrame
+
+'''Create new Group'''
+groupName = "New Group Layer"
+allLayers = arcpy.mapping.ListLayers(mxd, groupName, df) [0]
+for layer in allLayers: 
+    arcpy.ApplySymbologyFromLayer_management(layer, "TILE_NAME_+_EXTENSION")
